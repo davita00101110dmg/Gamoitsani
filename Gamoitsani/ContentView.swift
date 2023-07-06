5aa5797f9f0e6a9ea0d7a95f22a6f464deaf25a8
Daviti Khvedelidze <daviti.khvedelidze.1@btu.edu.ge>
2023-07-06 23:52:29 +0400

    Initial Commit

---
+++ Gamoitsani/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Gamoitsani
+//
+//  Created by Daviti Khvedelidze on 06.07.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gamoitsani/GamoitsaniApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamoitsaniApp.swift
+//  Gamoitsani
+//
+//  Created by Daviti Khvedelidze on 06.07.23.
+//
+
+import SwiftUI
+
+@main
+struct GamoitsaniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
