edee571a6436f97244ac6e4e02ddbcba60935f34
Daviti Khvedelidze <daviti.khvedelidze.1@btu.edu.ge>
2024-04-22 17:06:45 +0400

    Initial Commit

---
+++ Gamoitsani/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gamoitsani
+//
+//  Created by Daviti Khvedelidze on 22/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
