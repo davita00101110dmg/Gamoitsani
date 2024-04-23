2ead9088327607920dddaad7bc972708c0a6abec
Daviti Khvedelidze <daviti.khvedelidze.1@btu.edu.ge>
2024-04-23 22:30:42 +0400

    removed unnecessary methods, changed coordinator type

--- Gamoitsani/Source/ApplicationLayer/AppDelegate/AppDelegate.swift
+++ Gamoitsani/Source/ApplicationLayer/AppDelegate/AppDelegate.swift
@@ -15,16 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        let navigationController = UINavigationController()
-        let coordinator = HomeCoordinator(navigationController: navigationController)
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        
-        window.rootViewController = navigationController
-        window.makeKeyAndVisible()
-        self.window = window
-        
-        coordinator.start()
-        
         return true
     }
 
