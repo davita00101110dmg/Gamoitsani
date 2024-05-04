6b938c8c6ba770c0cb09c931ac6d3c58f6f75514
Daviti Khvedelidze <daviti.khvedelidze.1@btu.edu.ge>
2024-05-05 03:02:28 +0400

    added custom table view

---
+++ Gamoitsani/Source/Common/Components/GMTableView.swift
@@ -0,0 +1,23 @@
+//
+//  GMTableView.swift
+//  Gamoitsani
+//
+//  Created by Daviti Khvedelidze on 04/05/2024.
+//  Copyright © 2024 Daviti Khvedelidze. All rights reserved.
+//
+
+import UIKit
+
+class GMTableView: UITableView {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if bounds.size != intrinsicContentSize {
+            invalidateIntrinsicContentSize()
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        layoutIfNeeded()
+        return contentSize
+    }
+}
