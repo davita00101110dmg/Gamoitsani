710a421fda93c36f0d60c9869db101fcf1d7ab04
Daviti Khvedelidze <daviti.khvedelidze.1@btu.edu.ge>
2024-05-07 00:28:08 +0400

    added game story singleton

---
+++ Gamoitsani/Source/Common/Models/Game/GameStory.swift
@@ -0,0 +1,40 @@
+//
+//  GameStory.swift
+//  Gamoitsani
+//
+//  Created by Daviti Khvedelidze on 06/05/2024.
+//  Copyright © 2024 Daviti Khvedelidze. All rights reserved.
+//
+
+import Foundation
+import Collections
+
+class GameStory {
+    static let shared = GameStory()
+    
+    private init() {}
+    
+    var numberOfRounds: Int = 1
+    var lengthOfRound: Double = 45
+    var words: [String] = []
+    
+    var currentRound: Int = 1
+    var teams: OrderedDictionary<String, Int> = [:]
+    
+    var playingSessionCount = 0
+    var maxTotalSessions = 0
+    var currentTeamIndex = 0
+    
+    func reset() {
+        numberOfRounds = 1
+        lengthOfRound = 45
+        words = []
+        
+        currentRound = 1
+        teams = [:]
+        
+        playingSessionCount = 0
+        maxTotalSessions = 0
+        currentTeamIndex = 0
+    }
+}
