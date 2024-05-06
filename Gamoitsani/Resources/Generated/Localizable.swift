1344c581a226d037ab043beb7916da45b288d6e3
Daviti Khvedelidze <daviti.khvedelidze.1@btu.edu.ge>
2024-05-07 00:54:15 +0400

    localization update

--- Gamoitsani/Resources/Generated/Localizable.swift
+++ Gamoitsani/Resources/Generated/Localizable.swift
@@ -7,8 +7,18 @@ import Foundation
 
 // swiftlint:disable explicit_type_interface identifier_name line_length nesting type_body_length type_name
 public enum L10n {
+    /// Add
+    public static var add: String { L10n.tr("Localizable", "add") }
     /// and
     public static var and: String { L10n.tr("Localizable", "and") }
+    /// Cancel
+    public static var cancel: String { L10n.tr("Localizable", "cancel") }
+    /// No
+    public static var no: String { L10n.tr("Localizable", "no") }
+    /// Ok
+    public static var ok: String { L10n.tr("Localizable", "Ok") }
+    /// Yes
+    public static var yesPolite: String { L10n.tr("Localizable", "yesPolite") }
 
     public enum App {
       /// Gamoitsani
@@ -17,9 +27,41 @@ public enum L10n {
 
     public enum Screen {
 
+      public enum Game {
+
+        public enum ConfirmationAlert {
+          /// In this case, the game session will be lost.
+          public static var message: String { L10n.tr("Localizable", "screen.game.confirmation_alert.message") }
+          /// Are you sure you want to go back??
+          public static var title: String { L10n.tr("Localizable", "screen.game.confirmation_alert.title") }
+        }
+      }
+
       public enum GameSettings {
         /// Game Settings
         public static var title: String { L10n.tr("Localizable", "screen.game_settings.title") }
+
+        public enum AddTeamAlert {
+          /// Enter team name
+          public static var message: String { L10n.tr("Localizable", "screen.game_settings.add_team_alert.message") }
+          /// Add team!
+          public static var title: String { L10n.tr("Localizable", "screen.game_settings.add_team_alert.title") }
+        }
+
+        public enum IncorrectGameSettingsNotEnoughTeams {
+          /// Not enough teams, you need to add at least 2 teams to start the game!
+          public static var message: String { L10n.tr("Localizable", "screen.game_settings.incorrect_game_settings_not_enough_teams.message") }
+        }
+
+        public enum IncorrectGameSettingsNotUniqueTeams {
+          /// Team names must be unique!
+          public static var message: String { L10n.tr("Localizable", "screen.game_settings.incorrect_game_settings_not_unique_teams.message") }
+        }
+
+        public enum IncorrectParameter {
+          /// Incorrect parameter
+          public static var title: String { L10n.tr("Localizable", "screen.game_settings.incorrect_parameter.title") }
+        }
 
         public enum RoundsAmount {
           /// Rounds Amount: %@
@@ -33,6 +75,11 @@ public enum L10n {
           public static func title( _ p1: String) -> String {
               return L10n.tr("Localizable", "screen.game_settings.rounds_length.title", p1)
           }
+        }
+
+        public enum StartGame {
+          /// Start Game
+          public static var title: String { L10n.tr("Localizable", "screen.game_settings.start_game.title") }
         }
 
         public enum Teams {
