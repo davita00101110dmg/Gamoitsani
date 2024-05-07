6f6d576b174d6c6a9f88540f8646eed01d1e06ff
Daviti Khvedelidze <daviti.khvedelidze.1@btu.edu.ge>
2024-05-07 22:23:38 +0400

    modified GMButton configure function

--- Gamoitsani/Source/Common/Components/GMButton.swift
+++ Gamoitsani/Source/Common/Components/GMButton.swift
@@ -30,34 +30,32 @@ final class GMButton: UIButton {
         layer.cornerRadius = self.cornerRadius
     }
     
-    public func configure(text: String) {
-
-        var configuration = UIButton.Configuration.filled()
+    public func configure(text: String, fontSize: CGFloat = 18, isCircle: Bool = false) {
         
-        configuration.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
-            var outgoing = incoming
-            outgoing.font = F.BPGNinoMtavruli.bold.font(size: 18)
-            return outgoing
+        if isCircle {
+            cornerRadius = frame.width / 2
+            titleLabel?.font = F.BPGNinoMtavruli.bold.font(size: fontSize)
+            backgroundColor = Asset.secondary.color
+        } else {
+            var configuration = UIButton.Configuration.filled()
+            
+            configuration.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+                var outgoing = incoming
+                outgoing.font = F.BPGNinoMtavruli.bold.font(size: fontSize)
+                return outgoing
+            }
+            
+            configuration.baseBackgroundColor = Asset.secondary.color
+            configuration.baseForegroundColor = UIColor.white
+            configuration.contentInsets = NSDirectionalEdgeInsets.init(top: 5, leading: 0, bottom: 0, trailing: 0)
+            configuration.cornerStyle = .large
+            self.configuration = configuration
         }
         
         setTitle(text, for: .normal)
-        
-        configuration.baseBackgroundColor = Asset.secondary.color
-        configuration.baseForegroundColor = UIColor.white
-        configuration.contentInsets = NSDirectionalEdgeInsets.init(top: 5, leading: 0, bottom: 0, trailing: 0)
-        configuration.cornerStyle = .large
-        self.configuration = configuration
-        
         layoutIfNeeded()
     }
-    
-    public func configure(text: String, isCircle: Bool = false) {
-        self.cornerRadius = frame.width / 2
-        setTitle(text, for: .normal)
-        titleLabel?.font = F.BPGNinoMtavruli.bold.font(size: 18)
-        backgroundColor = Asset.secondary.color
-        layoutIfNeeded()
-    }
+
 }
 
 private extension GMButton {
