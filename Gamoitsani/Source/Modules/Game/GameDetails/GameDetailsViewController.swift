51b2c8bec205b5e2ca7003ceef94e24072970bc1
Daviti Khvedelidze <daviti.khvedelidze.1@btu.edu.ge>
2024-05-19 02:36:04 +0400

    changed name of variable

--- Gamoitsani/Source/Modules/Game/GameDetails/GameDetailsViewController.swift
+++ Gamoitsani/Source/Modules/Game/GameDetails/GameDetailsViewController.swift
@@ -32,7 +32,7 @@ final class GameDetailsViewController: BaseViewController<GameDetailsCoordinator
     var viewModel: GameDetailsViewModel?
     
     override func viewDidLoad() {
-        customBackBarButtonItem = true
+        shouldUseCustomBackBarButtonItem = true
         super.viewDidLoad()
         setupTableView()
         configureDataSource()
