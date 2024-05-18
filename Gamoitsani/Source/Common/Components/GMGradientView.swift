2cc54507f6b1d5553bd8264f467b740752712dd3
Daviti Khvedelidze <daviti.khvedelidze.1@btu.edu.ge>
2024-05-18 19:20:38 +0400

    added gm gradient view

---
+++ Gamoitsani/Source/Common/Components/GMGradientView.swift
@@ -0,0 +1,40 @@
+//
+//  GMGradientView.swift
+//  Gamoitsani
+//
+//  Created by Daviti Khvedelidze on 18/05/2024.
+//  Copyright © 2024 Daviti Khvedelidze. All rights reserved.
+//
+
+import UIKit
+
+final class GradientView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    private func setupView() {
+        autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        guard let gradientLayer = layer as? CAGradientLayer else { return }
+        
+        gradientLayer.colors = [Asset.gradientColor1.color.cgColor,
+                                Asset.gradientColor2.color.cgColor,
+                                Asset.gradientColor3.color.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: -0.5)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.locations = [0, 0.47, 1]
+        gradientLayer.frame = bounds
+    }
+    
+    override class var layerClass: AnyClass {
+        CAGradientLayer.self
+    }
+}
