0a254c14b564cc0aae332413c05f2a790ea321a5
Daviti Khvedelidze <daviti.khvedelidze.1@btu.edu.ge>
2024-08-13 04:15:41 +0400

    added new meta sdk, updated info plist

--- Gamoitsani/Source/ApplicationLayer/SceneDelegate.swift
+++ Gamoitsani/Source/ApplicationLayer/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FacebookCore
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -32,5 +33,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.makeKeyAndVisible()
         
         coordinator?.start()
+    }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url else {
+            return
+        }
+        
+        ApplicationDelegate.shared.application(
+            UIApplication.shared,
+            open: url,
+            sourceApplication: nil,
+            annotation: [UIApplication.OpenURLOptionsKey.annotation]
+        )
     }
 }
