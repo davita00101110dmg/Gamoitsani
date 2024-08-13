93679fb6310625e2346d20f3074210f212c491f5
Daviti Khvedelidze <daviti.khvedelidze.1@btu.edu.ge>
2024-08-13 04:16:14 +0400

    updated uiiview extension

--- Gamoitsani/Source/Common/Utils/Extensions/UIViewExtensions.swift
+++ Gamoitsani/Source/Common/Utils/Extensions/UIViewExtensions.swift
@@ -20,4 +20,11 @@ extension UIView {
         }
         return subviews
     }
+    
+    func asImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
+     }
 }
