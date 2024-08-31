4220919206c0ab2f3f695a1b99b7d5f5027740ef
Daviti Khvedelidze <daviti.khvedelidze.1@btu.edu.ge>
2024-08-31 16:21:44 +0400

    added empty test file

---
+++ GamoitsaniTests/GamoitsaniTests.swift
@@ -0,0 +1,9 @@
+//
+//  GamoitsaniTests.swift
+//  GamoitsaniTests
+//
+//  Created by Daviti Khvedelidze on 28/08/2024.
+//  Copyright © 2024 Daviti Khvedelidze. All rights reserved.
+//
+
+import Foundation
